81419c861775cfb58d85a4a4025493216e3cbbe5
Sergio Marotco <sergiomarotco@gmail.com>
2017-09-22 00:23:17 +0700

    add text to Form1.text

--- bulk_ip_lookup/Form1.Designer.cs
+++ bulk_ip_lookup/Form1.Designer.cs
@@ -247,6 +247,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP bulk lookup tool";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
