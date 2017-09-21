d496cda23da19ccf2eb8da8f0d8a0f011154e2d8
Sergio Marotco <sergiomarotco@gmail.com>
2017-09-21 22:33:00 +0700

    Добавьте файлы проекта.

---
+++ bulk_ip_lookup/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace IP_Bulk_lookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.скопироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отменаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progresslabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(305, 495);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(90, 13);
+            this.linkLabel2.TabIndex = 0;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Icons icons8.com";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel2_LinkClicked);
+            // 
+            // listView2
+            // 
+            this.listView2.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView2.AllowDrop = true;
+            this.listView2.CheckBoxes = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(12, 12);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(284, 496);
+            this.listView2.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.ListView2_SelectedIndexChanged);
+            this.listView2.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListView2_DragDrop);
+            this.listView2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListView2_MouseClick);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ip";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "NAME";
+            this.columnHeader4.Width = 1500;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::IP_Bulk_lookup.Properties.Resources.upload;
+            this.pictureBox4.Location = new System.Drawing.Point(308, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::IP_Bulk_lookup.Properties.Resources.Select;
+            this.pictureBox5.Location = new System.Drawing.Point(308, 118);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.PictureBox5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Drag / drop file";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(317, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Select cells";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(317, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "RM to copy";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.скопироватьToolStripMenuItem,
+            this.отменаToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(147, 48);
+            // 
+            // скопироватьToolStripMenuItem
+            // 
+            this.скопироватьToolStripMenuItem.Name = "скопироватьToolStripMenuItem";
+            this.скопироватьToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.скопироватьToolStripMenuItem.Text = "Скопировать";
+            this.скопироватьToolStripMenuItem.Click += new System.EventHandler(this.СкопироватьToolStripMenuItem_Click);
+            // 
+            // отменаToolStripMenuItem
+            // 
+            this.отменаToolStripMenuItem.Name = "отменаToolStripMenuItem";
+            this.отменаToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.отменаToolStripMenuItem.Text = "Отмена";
+            this.отменаToolStripMenuItem.Click += new System.EventHandler(this.ОтменаToolStripMenuItem_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::IP_Bulk_lookup.Properties.Resources.MouseRight;
+            this.pictureBox6.Location = new System.Drawing.Point(308, 224);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 4;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IP_Bulk_lookup.Properties.Resources.export;
+            this.pictureBox1.Location = new System.Drawing.Point(308, 323);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Export";
+            // 
+            // progresslabel
+            // 
+            this.progresslabel.AutoSize = true;
+            this.progresslabel.Location = new System.Drawing.Point(305, 482);
+            this.progresslabel.Name = "progresslabel";
+            this.progresslabel.Size = new System.Drawing.Size(0, 13);
+            this.progresslabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 520);
+            this.Controls.Add(this.progresslabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.linkLabel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem скопироватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отменаToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label progresslabel;
+    }
+}
+
---
+++ bulk_ip_lookup/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_Bulk_lookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void InsertIntoListDelegate(ListViewItem item);
+
+        public void InsertIntoList(ListViewItem item)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new InsertIntoListDelegate(InsertIntoList), item);
+            }
+            else
+            {
+                listView2.Items.Add(item);
+            }
+        }
+        int progress = 0;
+        static bool CheckIp(string address)
+        {
+            var nums = address.Split('.');
+            int useless;
+            return nums.Length == 4 && nums.All(n => int.TryParse(n, out useless)) && nums.Select(int.Parse).All(n => n < 256);
+        }
+        private void Progress_change(int step)
+        {
+            lock (locker)
+            {
+                progress = progress + step;
+                if (progress != 0)
+                    Invoke((ThreadStart)delegate { progresslabel.Text = progress.ToString(); });
+                //progresslabel.Text = progress.ToString();
+                else Invoke((ThreadStart)delegate { progresslabel.Text = ""; });
+            }
+        }
+        private void GetHostEntry(object IP)
+        {
+            //Progress_change(1);                    
+            string[] y = new string[2];
+            try
+            {
+                y[0] = (string)IP;
+                try
+                {
+                    string ip = ((string)IP);
+                    char[] trimcharachters = { ' ', '\t' };
+                    ip = ip.Trim(trimcharachters);
+                    if (CheckIp(ip))
+                    {
+                        y[1] = Dns.GetHostEntry(ip).HostName;
+                        y[0] = ip;
+                    }
+                    else
+                    {
+                        y[1] = "incorrect IP";
+                    }
+                }
+                catch (Exception ee)
+                {
+                    y[1] = ee.Message;
+                }
+                Task.Factory.StartNew(() => InsertIntoList(new ListViewItem(y)));
+            }
+            catch { }
+            Progress_change(-1);
+        }
+
+        private void LinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ru.icons8.com");
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Icon = Properties.Resources.Icon;
+            this.AllowDrop = true;
+        }
+        static object locker = new object();
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            listView2.Items.Clear();
+            Array data1 = e.Data.GetData("FileName") as Array;
+            //string filename = String.Empty;
+            if (data1 != null)
+            {
+                if ((data1.Length == 1) && (data1.GetValue(0) is String))
+                {
+                    string filename = ((string[])data1)[0];
+                    //string ext = Path.GetExtension(filename).ToLower();
+                    string[] lines = File.ReadAllLines(filename);
+                    try
+                    {
+                        progress = lines.Length;
+                        Invoke((ThreadStart)delegate { progresslabel.Text = lines.Length.ToString(); });
+                        for (int i = 0; i < lines.Length; i++)
+                        {
+                            Thread myThread = new Thread(new ParameterizedThreadStart(GetHostEntry));
+                            myThread.Start(lines[i]);
+                        }
+                    }
+                    catch { }
+                }
+            }
+            listView2.Sort();
+            listView2.Refresh();
+        }
+
+        private void ListView2_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right:
+                    {
+                        contextMenuStrip1.Show(this, new Point(e.X, e.Y));
+                    }
+                    break;
+            }
+        }
+
+        private void ОтменаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Hide();
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+            //listView2.select
+        }
+
+        private void ListView2_DragDrop(object sender, DragEventArgs e)
+        {
+            listView2.Items.Clear();
+            Array data1 = e.Data.GetData("FileName") as Array;
+            //string filename = String.Empty;
+            if (data1 != null)
+            {
+                if ((data1.Length == 1) && (data1.GetValue(0) is String))
+                {
+                    string filename = ((string[])data1)[0];
+                    //string ext = Path.GetExtension(filename).ToLower();
+                    string[] lines = File.ReadAllLines(filename);
+                    try
+                    {
+                        for (int i = 0; i < lines.Length; i++)
+                        {
+                            Thread myThread = new Thread(new ParameterizedThreadStart(GetHostEntry));
+                            myThread.Start(lines[i]);
+                        }
+                    }
+                    catch { }
+                }
+            }
+            listView2.Sort();
+            listView2.Refresh();
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            string lines = string.Empty;
+            for (int i = 0; i < listView2.SelectedItems.Count; i++)
+            {
+                lines += listView2.SelectedItems[i].SubItems[0].Text+Environment.NewLine;
+            }
+            if (lines != "")
+                Clipboard.SetText(lines);
+        }
+
+        private void ListView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string lines = string.Empty;
+            for (int i = 0; i < listView2.SelectedItems.Count; i++)
+            {
+                lines += listView2.SelectedItems[i].SubItems[0].Text + Environment.NewLine;
+            }
+            if(lines!="")
+                Clipboard.SetText(lines);
+        }
+
+        private void СкопироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string lines = string.Empty;
+            for (int i = 0; i < listView2.SelectedItems.Count; i++)
+            {
+                lines += listView2.SelectedItems[i].SubItems[0].Text + Environment.NewLine;
+            }
+            if (lines != "")
+                Clipboard.SetText(lines);
+        }
+    }
+}
